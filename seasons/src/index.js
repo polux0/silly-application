b9e4ade7006d5422fe822ea267981dc4a1f5e20a
Aleksa Stojanović <afidelo@protonmail.com>
2019-03-08 20:20:25 +0100

    SeasonDisplay class based component, introducing state, introducing node.js api

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
+// const App = () => {
 
+// 	// function success(success){
+// 	// 	console.log('Successfully aquired geolocation => ' + success);
+// 	// }
+// 	// function error(error){
+// 	// 	console.log('There is an error when trying to aquire geolocation => ' + error);
+// 	// }
 
-const App = () => {
-	return <div> Hello there, from a container </div>;
-};
+// 	// navigator.geolocation.getCurrentPostion(error, success);
+
+// 	return (
+// 		<div> Hello there, from a container 
+		
+// 		</div>
+// 	)
+// };
+class App extends React.Component {
+	render(){
+		return (
+		<div> Lattitude
+			<SeasonDisplay/>
+		</div>
+		) 
+	}
+}
 
 ReactDOM.render(
 	<App />,
